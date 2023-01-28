5dd7fdf085d3fec572115272b0a2c0cdb3e654b7
sergioescobarh <sergio.escobar.henao@gmail.com>
2023-01-28 00:23:35 -0500

    statefull products and starting of API consumption

--- react-g5/src/components/products/Products.jsx
+++ react-g5/src/components/products/Products.jsx
@@ -1,5 +1,30 @@
+import React, { Component } from 'react';
 import "../products/products.css";
-function Products() {
+class Products extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            image:''
+        };
+    }
+    list = (data) => {
+        console.log(data);
+    }
+    apiCall(url,consecuencia){
+        fetch(url)
+        .then(response => response.json())
+        .then(data => consecuencia(data))
+        .catch(error => console.log(error))
+    }
+    componentDidMount(){
+        console.log('me monté');
+        this.apiCall('http://localhost:3001/products/list',this.list);
+    }
+    componentDidUpdate(){
+        console.log('me actualizé');
+    }
+    
+    render(){
     return (
         <div className="products-container_images">
             <img className="product-image" src="../../../assets/productsImages/1.png" alt="" />
@@ -8,6 +33,6 @@ function Products() {
             <img className="product-image" src="../../../assets/productsImages/4.png" alt="" />
         </div>
     );
-}
+}}
 
 export default Products;
