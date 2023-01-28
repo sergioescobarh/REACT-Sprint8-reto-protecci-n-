37475d40e44d186f5665e1be844bf635a4cf9f12
sergioescobarh <sergio.escobar.henao@gmail.com>
2023-01-28 17:30:02 -0500

    test 4.0

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../products/products.css";
 function Products() {
 //   const [listProducts, setListProducts] = useState([]);
