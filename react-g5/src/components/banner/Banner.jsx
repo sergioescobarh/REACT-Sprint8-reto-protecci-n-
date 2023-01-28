70de6bd8137e1aeaeba8df36faac182902c1b446
Diana Arroyave <dianaarroyave4@gmail.com>
2023-01-28 15:06:23 -0500

    CSS and visual

--- react-g5/src/components/banner/Banner.jsx
+++ react-g5/src/components/banner/Banner.jsx
@@ -2,7 +2,7 @@ import '../banner/banner.css'
 function Banner() {
     return (
         <div className="banner-container">
-            <img src='../../../assets/pagesImages/banner.png' className="banner-img" alt="logo" />
+            <img src='../../../../assets/pagesImages/bannerPromoReact.png' className="banner-img" alt="logo" />
         </div>
     )
 };
--- react-g5/src/components/rulesParticipation/Rules.jsx
+++ react-g5/src/components/rulesParticipation/Rules.jsx
@@ -8,10 +8,32 @@ function Rules() {
             <div className="rules-container_description">
                 <h2 className="rules-title">¿Cómo participar?</h2>
                 <p className="rules-description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-                    architecto alias quos perspiciatis deleniti iusto. Vel minima
-                    assumenda praesentium quae cum, quasi repudiandae, tenetur quos eius
-                    ducimus fuga rerum. Temporibus.
+                    <ol>
+                        <li>
+                        Para participar, los interesados deben registrarse en esta página web y proporcionar información personal veraz y precisa.
+                        </li>
+                        <li>
+                        Cada compra realizada en nuestra tienda en línea TEAL durante el período del concurso, contará como una entrada para el sorteo.
+                        </li>
+                        <li>
+                        Los participantes pueden ganar hasta un 60% de descuento en la nueva colección con acceso exclusivo, en función del número de compras realizadas.
+                        </li>
+                        <li>
+                        El sorteo se llevará a cabo el 20 de febrero de 2023 y el ganador será notificado a través de correo electrónico o teléfono.
+                        </li>
+                        <li>
+                        El premio no es canjeable por dinero en efectivo.
+                        </li>
+                        <li>
+                        El personal de TEAL y sus familiares directos no están elegibles para participar en el concurso.
+                        </li>
+                        <li>
+                        Al registrarse para el concurso, los participantes aceptan los términos y condiciones del mismo.
+                        </li>
+                        <li>
+                        Al participar en este concurso, los participantes aceptan permitir que TEAL publique su nombre y fotografía en caso de ser el ganador del concurso.
+                        </li>
+                    </ol>
                 </p>
             </div>
         </div>
