1f86104e6ec569650a5d837a58933550dc96cddb
sergioescobarh <sergio.escobar.henao@gmail.com>
2023-01-28 17:27:31 -0500

    test 3.0

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,38 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
 import "../products/products.css";
-class Products extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            image:''
-        };
-    }
-    list = (data) => {
-        console.log(data);
-    }
-    apiCall(url,consecuencia){
-        fetch(url)
-        .then(response => response.json())
-        .then(data => consecuencia(data))
-        .catch(error => console.log(error))
-    }
-    componentDidMount(){
-        console.log('me monté');
-        this.apiCall('http://localhost:3001/products/list',this.list);
-    }
-    componentDidUpdate(){
-        console.log('me actualizé');
-    }
-    
-    render(){
-    return (
-        <div className="products-container_images">
-            <img className="product-image" src="../../../assets/productsImages/1.png" alt="" />
-            <img className="product-image" src="../../../assets/productsImages/2.png" alt="" />
-            <img className="product-image" src="../../../assets/productsImages/3.png" alt="" />
-            <img className="product-image" src="../../../assets/productsImages/4.png" alt="" />
-        </div>
-    );
-}}
+function Products() {
+//   const [listProducts, setListProducts] = useState([]);
+//   console.log(listProducts);
+//   const apiCall = () => {
+//     fetch("https://productsapi-production-d23f.up.railway.app/products/list", {
+//       headers: {
+//         "Access-Control-Allow-Origin": true,
+//         "Content-Type": "application/json",
+//       },
+//     })
+//       .then((response) => console.log(response.json()))
+//       .then((data) => console.log(data.data))
+//       .catch((error) => console.log(error));
+//   };
+//   console.log(listProducts);
+//   useEffect(() => {
+//     apiCall();
+//   }, []);
+  return (
+    <div className="products-container_images">
+      <img
+        className="product-image"
+        src="../../../assets/productsImages/1.png"
+        alt=""
+      />
+      <img
+        className="product-image"
+        src="../../../assets/productsImages/2.png"
+        alt=""
+      />
+      <img
+        className="product-image"
+        src="../../../assets/productsImages/3.png"
+        alt=""
+      />
+      <img
+        className="product-image"
+        src="../../../assets/productsImages/4.png"
+        alt=""
+      />
+    </div>
+  );
+}
+// class Products extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             image:''
+//         };
+//     }
+//     list = (data) => {
+//         console.log(data);
+//     }
+//     apiCall(url,consecuencia){
+//         fetch(url)
+//         .then(response => response.json())
+//         .then(data => consecuencia(data))
+//         .catch(error => console.log(error))
+//     }
+//     componentDidMount(){
+//         console.log('me monté');
+//         this.apiCall('http://localhost:3001/products/list',this.list);
+//     }
+//     componentDidUpdate(){
+//         console.log('me actualizé');
+//     }
+
+//     render(){
+//     return (
+//         <div className="products-container_images">
+//             <img className="product-image" src="../../../assets/productsImages/1.png" alt="" />
+//             <img className="product-image" src="../../../assets/productsImages/2.png" alt="" />
+//             <img className="product-image" src="../../../assets/productsImages/3.png" alt="" />
+//             <img className="product-image" src="../../../assets/productsImages/4.png" alt="" />
+//         </div>
+//     );
+// }}
 
 export default Products;
