4f018d8428795c29307ac7cc900d2ca04883e8b7
william serna <wserna880@gmail.com>
2023-01-28 20:45:55 -0500

    add listUser API

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,26 +2,23 @@ import React, { useState, useEffect } from "react";
 import "../products/products.css";
 function Products() {
   const [listProducts, setListProducts] = useState([]);
-  console.log(listProducts);
+  // console.log(listProducts);
   const apiCall = async() => {
     await fetch("https://productsapi-production-d23f.up.railway.app/products/list", {
       headers: {
         "Access-Control-Allow-Origin": "*",
         'Content-Type': 'application/json'
-        
-
       }
       
     })
       .then(response => response.json())
-       .then((data) => setListProducts(data.data))
+      .then((data) => setListProducts(data.data))
       .catch((error) => console.log(error));
   };
   
-  console.log(listProducts);
+  // console.log(listProducts);
   
   useEffect(() => {
-    
     apiCall();
   }, []);
   return (
--- src/components/rancking/Rancking.jsx
+++ src/components/rancking/Rancking.jsx
@@ -1,28 +1,41 @@
-import '../rancking/rancking.css'
-function Rancking () {
-    return (
-        <div className='rancking-container'>
-            <h2 className='title-rancking'>Rancking de clientes</h2>
-            <div className="rancking-user_container">
-                <h3 className='rancking-user_position'>1</h3>
-                <img className='rancking-user_image' src="../../../assets/pagesImages/user.png" alt="user" />
-                <p className='rancking-user_name'>Nombre usuario</p>
-                <h3 className='rancking-user_sale'>60%</h3>
-            </div>
-            <div className="rancking-user_container">
-                <h3 className='rancking-user_position'>2</h3>
-                <img className='rancking-user_image' src="../../../assets/pagesImages/user.png" alt="user" />
-                <p className='rancking-user_name'>Nombre usuario</p>
-                <h3 className='rancking-user_sale'>50%</h3>
-            </div>
-            <div className="rancking-user_container">
-                <h3 className='rancking-user_position'>3</h3>
-                <img className='rancking-user_image' src="../../../assets/pagesImages/user.png" alt="user" />
-                <p className='rancking-user_name'>Nombre usuario</p>
-                <h3 className='rancking-user_sale'>40%</h3>
-            </div>
-        </div>
-    )
-};
+import React, { useState, useEffect } from "react";
+
+import "../rancking/rancking.css";
+function Rancking() {
+  const [listUsers, setListUsers] = useState([]);
+  const getListUsers = async () => {
+    await fetch("https://teal-api.up.railway.app/users", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setListUsers(data.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getListUsers();
+  }, []);
+
+  return (
+    <div className="rancking-container">
+      <h2 className="title-rancking">Rancking de clientes</h2>
+      {listUsers.map((users, i) => (
+          <div className="rancking-user_container">
+            <h3 className="rancking-user_position">{i+1}</h3>
+            <img
+              className="rancking-user_image"
+              src="../../../assets/pagesImages/user.png"
+              alt="user"
+            />
+            <p className="rancking-user_name">{users.firtName + " " + users.lastName}</p>
+            <h3 className="rancking-user_sale">60%</h3>
+          </div>
+        ))}
+    </div>
+  );
+}
 
 export default Rancking;
