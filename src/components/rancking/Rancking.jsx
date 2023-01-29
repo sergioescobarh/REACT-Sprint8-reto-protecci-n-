670a85b8f80872daed71672fd6c39ebe50b8b0cc
william serna <wserna880@gmail.com>
2023-01-28 21:48:01 -0500

    update percentage

--- src/components/rancking/Rancking.jsx
+++ src/components/rancking/Rancking.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import "../rancking/rancking.css";
 function Rancking() {
   const [listUsers, setListUsers] = useState([]);
+  const userCuantity = listUsers.length;
+
   const getListUsers = async () => {
     await fetch("https://teal-api.up.railway.app/users", {
       headers: {
@@ -22,18 +24,24 @@ function Rancking() {
   return (
     <div className="rancking-container">
       <h2 className="title-rancking">Rancking de clientes</h2>
-      {listUsers.map((users, i) => (
+      {listUsers.map((users, i) => {
+        let percentage = (userCuantity - i) * 10;
+        percentage = percentage>=60?60:percentage;
+        return (
           <div className="rancking-user_container">
-            <h3 className="rancking-user_position">{i+1}</h3>
+            <h3 className="rancking-user_position">{i + 1}</h3>
             <img
               className="rancking-user_image"
               src="../../../assets/pagesImages/user.png"
               alt="user"
             />
-            <p className="rancking-user_name">{users.firtName + " " + users.lastName}</p>
-            <h3 className="rancking-user_sale">60%</h3>
+            <p className="rancking-user_name">
+              {users.firtName + " " + users.lastName}
+            </p>
+            <h3 className="rancking-user_sale">{percentage}%</h3>
           </div>
-        ))}
+        );
+      })}
     </div>
   );
 }
